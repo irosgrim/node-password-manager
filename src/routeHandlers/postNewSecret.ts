8ec531a9b4bdc5761662596eaa3800696ca56117
ion rosgrim <jrunin@gmail.com>
2021-04-23 22:49:05 +0200

    attach s3 file path to entry

--- src/routeHandlers/postNewSecret.ts
+++ src/routeHandlers/postNewSecret.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Multer } from 'multer';
 import Cryptography from '../crypto/crypto';
 import { pool } from '../db/connect';
 import { getCryptoKeyForUser, insertNewSecret } from '../db/queries';
@@ -26,38 +27,23 @@ export const postNewSecret = () => {
         const files = req.files;
         console.log(files);
 
-       if(files.length) {
-        files.forEach(file => {
-            const timeInMilliseconds = new Date().getTime();
-            const renamedFile = timeInMilliseconds +'_' + authorisedUser +'_' + file.originalname;
-            minioClient.putObject('testing', renamedFile, file.buffer, (error, etag) => {
-                if(error) {
-                    return log.error(req, error);
-                }
-                console.log('upload succesful!');
-            });
-
-            minioClient.presignedUrl('GET', 'testing', renamedFile, 60*60*24, (err, presignedUrl) => {
-                if (err) {
-                    return console.log(err)
-                }
-                console.log(presignedUrl)
-              })
-        })
-       }
-    
+        
         if (secret) {
+            let filesPaths: string[] | null = null; 
+            if(files.length) {
+                 filesPaths = await uploadFiles(authorisedUser, files);
+            }
             const cryptography = new Cryptography();
             const cryptoKeyForUser = await pool.query(getCryptoKeyForUser, [authorisedUser]);
             const cryptoKey = cryptoKeyForUser.rows[0].key;
             const secretEncrypted = await cryptography.encrypt(secret, cryptoKey);
             try {
-                const newSecretResponse = await pool.query(insertNewSecret, [label, secretEncrypted, authorisedUser, icon, category]);
+                const newSecretResponse = await pool.query(insertNewSecret, [label, secretEncrypted, authorisedUser, icon, category, filesPaths]);
                 if(attachments) {
                     const {id, user_id, date_created } = newSecretResponse.rows[0];
                     console.log([id, user_id, date_created]);
                     const millis = new Date(date_created).getTime();
-                    console.log(uploadSecretAttachments(user_id, id, millis, attachments));
+                    // console.log(uploadSecretAttachments(user_id, id, millis, attachments));
                 }
                 res.send('OK!');
             } catch(err) {
@@ -66,3 +52,26 @@ export const postNewSecret = () => {
         }
     }
 }
+
+export async function uploadFiles(authorisedUser: number, files: Express.Multer.File[]) {
+    let paths: string[] = [];
+    for(const file of files) {
+        const timeInMilliseconds = new Date().getTime();
+        const renamedFile = timeInMilliseconds +'_' + authorisedUser +'_' + file.originalname;
+        const r = await minioClient.putObject('testing', renamedFile, file.buffer);
+        paths = [...paths, renamedFile];
+    }
+    return paths;
+}
+
+export async function getPresignedFilesUrl(files: string[]): Promise<string[]> {
+    let urls: string[] = [];
+    const oneHour = 60 * 60;
+    for(const file of files) {
+        const getPresignedUrl = await minioClient.presignedUrl('GET', 'testing', file, oneHour);
+        urls = [...urls, getPresignedUrl];
+    }
+    return urls;
+
+}
+
